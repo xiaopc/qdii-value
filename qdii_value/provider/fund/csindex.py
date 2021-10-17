3c58079949c248da8cc686238ed20e15d4cf2e74
xiaopc <xiaopc@users.noreply.github.com>
2021-10-17 22:35:01 +0800

    0.3.32 1. update csindex source

--- qdii_value/provider/fund/csindex.py
+++ qdii_value/provider/fund/csindex.py
@@ -1,14 +1,24 @@
 import requests
 import xlrd
 from datetime import datetime
-from bs4 import BeautifulSoup
 import re
 
-__url = 'http://www.csindex.com.cn/zh-CN/indices/index-detail/{}'
-__url_xls = 'http://www.csindex.com.cn/uploads/file/autofile/closeweight/{}closeweight.xls'
+__url_basic = 'https://www.csindex.com.cn/csindex-home/indexInfo/index-basic-info/{}'
+__url_top = 'https://www.csindex.com.cn/csindex-home/index/weight/top10/{}'
+__url_xls = 'https://csi-web-dev.oss-cn-shanghai-finance-1-pub.aliyuncs.com/static/html/csindex/public/uploads/file/autofile/closeweight/{}closeweight.xls'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'
 }
+
+
+def get_json(url):
+    rsp = requests.get(url, headers=headers)
+    if r.status_code != 200:
+        return None
+    rsp = rsp.json()
+    if rsp['code'] != '200':
+        return None
+    return rsp['data']
 
 
 def lists(fund_id):
@@ -17,38 +27,35 @@ def lists(fund_id):
         book = xlrd.open_workbook(file_contents=r.content)
         sheet = book.sheet_by_index(0)
         r = {
-            "fund_name": sheet.cell_value(1, 2),
-            "last_update": datetime(*xlrd.xldate_as_tuple(sheet.cell_value(1, 0), book.datemode)).strftime('%Y-%m-%d'),
-            "equities": []
+            'fund_name': sheet.cell_value(1, 2),
+            'last_update': datetime.strptime(sheet.cell_value(1, 0), '%Y%m%d').strftime('%Y-%m-%d'),
+            'equities': []
         }
         for i in range(1, sheet.nrows):
             line = sheet.row_values(i)
-            r["equities"].append({
+            is_duplicate = filter(lambda l: l['code'] == line[4], r['equities'])
+            if len(list(is_duplicate)) > 0:
+                continue
+            r['equities'].append({
                 'code': line[4],
                 'name': line[5],
-                'weight': line[8]
+                'weight': line[9]
             })
         return r
 
     # else fetch 10
-    r = requests.get(__url.format(str(fund_id)), headers=headers)
-    if r.status_code != 200:
+    r = get_json(__url_basic.format(str(fund_id)))
+    if r is None:
         return None
-    s = BeautifulSoup(r.content, features='lxml')
-    n = s.find('h1')
-    if not n:
+    fund_name = r['indexFullNameCn']
+    r = get_json(__url_top.format(str(fund_id)))
+    if r is None:
         return None
-    d = s.find('h2', text='十大权重股')
-    if not d:
-        return None
-    # d = re.search(r"\d{4}-\d{2}-\d{2}", d).group()
-    p = list(filter(lambda node: node.name == 'table', d.next_siblings))[0]
 
-    def get_tr(tr):
-        td = tr.find_all('td')
+    def get_tr(row):
         return {
-            'code': td[0].string,
-            'name': td[1].string,
-            'weight': td[3].string
+            'code': row['securityCode'],
+            'name': row['securityName'],
+            'weight': row['weight']
         }
-    return {"fund_name": n.text, "last_update": d.next_sibling.text.split(':')[1], "equities": list(map(get_tr, p.tbody.find_all('tr')))}
+    return {'fund_name': fund_name, 'last_update': r['updateDate'], 'equities': list(map(get_tr, r['weightList']))}
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
 
 setup(
     name='qdii_value',
-    version='0.3.31',
+    version='0.3.32',
     description='计算 QDII 基金估值',
     classifiers=[
         'Development Status :: 4 - Beta',
